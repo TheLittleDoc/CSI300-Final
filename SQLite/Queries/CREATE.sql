5564f8f5c716ba2c74bb8e0894b17c45c5a52667
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2025-04-09 21:36:15 -0400

    Created SQLite db and CREATE queries
    - Joe and Ely

---
+++ SQLite/Queries/CREATE.sql
@@ -0,0 +1,65 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE Course (
+    CourseID        INTEGER     PRIMARY KEY ASC AUTOINCREMENT
+                                UNIQUE
+                                NOT NULL,
+    CoursePrefix    TEXT (4)    NOT NULL,
+    CourseCode      INTEGER (3) NOT NULL,
+    CourseName      TEXT        NOT NULL,
+    Description     TEXT
+);
+
+CREATE TABLE Section (
+    SectionID       INTEGER     PRIMARY KEY AUTOINCREMENT
+                                UNIQUE
+                                NOT NULL,
+    CourseID        INTEGER     REFERENCES Course (CourseID) ON DELETE CASCADE
+                                NOT NULL,
+    Weekday         INTEGER (1) NOT NULL
+                                CONSTRAINT CheckWeekday CHECK ( (Weekday >= 0) AND
+                                                                (Weekday < 7) ),
+    MeetingTime     INTEGER (4) CHECK (MeetingTime < 2400) 
+);
+
+CREATE TABLE Assignment (
+    AssignmentID    INTEGER     PRIMARY KEY AUTOINCREMENT
+                                UNIQUE
+                                NOT NULL,
+    Name            TEXT        NOT NULL,
+    SectionID       INTEGER     REFERENCES Section (SectionID) ON DELETE CASCADE
+                                NOT NULL,
+    Description     TEXT        NOT NULL,
+    MaterialURL     TEXT        NOT NULL
+);
+
+CREATE TABLE Student (
+    StudentID       INTEGER     PRIMARY KEY AUTOINCREMENT
+                                UNIQUE
+                                NOT NULL,
+    FirstName       TEXT        NOT NULL,
+    LastName        TEXT        NOT NULL,
+    Email           TEXT        NOT NULL
+                                UNIQUE
+                                CONSTRAINT CheckEmail CHECK ( Email LIKE ('%_@__%.__%')),
+    GraduationYear  INTEGER     NOT NULL
+);
+
+CREATE TABLE Enrollment (
+    StudentID       INTEGER     NOT NULL
+                                REFERENCES Student (StudentID) ON DELETE CASCADE,
+    
+
+    SectionID       INTEGER     NOT NULL
+                                REFERENCES Section (SectionID) ON DELETE CASCADE,
+    PRIMARY KEY (StudentID, SectionID) 
+);
+
+CREATE TABLE Grade (
+    StudentID       INTEGER     REFERENCES Student (StudentID)
+                                NOT NULL,
+    AssignmentID    INTEGER     REFERENCES Assignment (AssignmentID)
+                                NOT NULL
+);
+
+ROLLBACK;
